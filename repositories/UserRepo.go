c6862af48fb4cc5f778d38896645442ada142c34
Zhanarys17 <zhanarys.d7@gmail.com>
2022-11-26 19:16:23 +0600

    added Postgresql database and package for saving DSN of postgres!

--- repositories/UserRepo.go
+++ repositories/UserRepo.go
@@ -13,3 +13,8 @@ func (m *UserModel) Create(user models.User) error {
 	result := m.Db.Create(&user)
 	return result.Error
 }
+
+func (m *UserModel) Find(user models.User) error {
+	result := m.Db.Find(&user)
+	return result.Error
+}
